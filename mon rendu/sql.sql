52fc963ded2ed3085b80b4f86c7807e43fcd7034
Beaufaye BAOUROU <baouroubeaufaye881@gmail.com>
2023-08-01 18:36:30 +0200

    correction projet

--- mon rendu/sql.sql
+++ mon rendu/sql.sql
@@ -1,24 +1,30 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     28/07/2023 11:59:07                          */
+/* Created on:     01/08/2023 18:35:54                          */
 /*==============================================================*/
 
 
 drop table if exists DM;
 
-drop table if exists calendrier;
+drop table if exists demande_soutenance;
 
 drop table if exists enseignant;
 
 drop table if exists etudiant;
 
+drop table if exists jury;
+
 drop table if exists maitre_de_stage;
 
 drop table if exists president_jury;
 
+drop table if exists role;
+
 drop table if exists salle;
 
 drop table if exists soutenances;
+
+drop table if exists utilisateur;
 
 /*==============================================================*/
 /* Table: DM                                                    */
@@ -26,7 +32,6 @@ drop table if exists soutenances;
 create table DM
 (
    id                   int not null,
-   etu_id               int not null,
    nom                  varchar(254),
    prenom               varchar(254),
    contact              int,
@@ -34,15 +39,15 @@ create table DM
 );
 
 /*==============================================================*/
-/* Table: calendrier                                            */
+/* Table: demande_soutenance                                    */
 /*==============================================================*/
-create table calendrier
+create table demande_soutenance
 (
    id                   int not null,
-   date                 datetime,
-   heure                int,
+   etu_id               int not null,
+   statut               varchar(254),
    primary key (id),
-   key AK_Identifiant_1 (id)
+   key AK_Identifier_1 (id)
 );
 
 /*==============================================================*/
@@ -51,8 +56,6 @@ create table calendrier
 create table enseignant
 (
    id                   int not null,
-   DM_id                int,
-   cal_id               int not null,
    nom                  varchar(254),
    prenom               varchar(254),
    sexe                 varchar(254),
@@ -67,7 +70,8 @@ create table enseignant
 create table etudiant
 (
    id                   int not null,
-   cal_id               int not null,
+   DM_id                int not null,
+   mai_id               int not null,
    nom                  varchar(254),
    prenom               varchar(254),
    sexe                 varchar(254),
@@ -78,12 +82,26 @@ create table etudiant
 );
 
 /*==============================================================*/
+/* Table: jury                                                  */
+/*==============================================================*/
+create table jury
+(
+   id                   int not null,
+   ens_id               int not null,
+   mai_id               int,
+   sou_id               int,
+   DM_id                int not null,
+   nom                  varchar(254),
+   primary key (id),
+   key AK_Identifier_1 (id)
+);
+
+/*==============================================================*/
 /* Table: maitre_de_stage                                       */
 /*==============================================================*/
 create table maitre_de_stage
 (
    id                   int not null,
-   etu_id               int not null,
    nom                  varchar(254),
    prenom               varchar(254),
    contact              int,
@@ -96,10 +114,20 @@ create table maitre_de_stage
 create table president_jury
 (
    id                   int not null,
-   sou_id               int not null,
-   cal_id               int not null,
    nom                  varchar(254),
    primary key (id)
+);
+
+/*==============================================================*/
+/* Table: role                                                  */
+/*==============================================================*/
+create table role
+(
+   id                   int not null,
+   uti_id               int,
+   "label"              varchar(254),
+   primary key (id),
+   key AK_Identifier_1 (id)
 );
 
 /*==============================================================*/
@@ -108,7 +136,6 @@ create table president_jury
 create table salle
 (
    id                   int not null,
-   sou_id               int not null,
    nom                  varchar(254),
    primary key (id),
    key AK_Identifiant_1 (id)
@@ -120,35 +147,59 @@ create table salle
 create table soutenances
 (
    id                   int not null,
-   etu_id               int not null,
+   dem_id               int not null,
+   pre_id               int not null,
+   sal_id               int not null,
    date                 datetime,
    primary key (id)
 );
 
-alter table DM add constraint FK_Association_3 foreign key (etu_id)
+/*==============================================================*/
+/* Table: utilisateur                                           */
+/*==============================================================*/
+create table utilisateur
+(
+   id                   int not null,
+   etu_id               int,
+   nom                  varchar(254),
+   prenom               varchar(254),
+   contact              int,
+   primary key (id)
+);
+
+alter table demande_soutenance add constraint FK_Association_26 foreign key (etu_id)
       references etudiant (id) on delete restrict on update restrict;
 
-alter table enseignant add constraint FK_Association_10 foreign key (DM_id)
+alter table etudiant add constraint FK_Association_22 foreign key (DM_id)
       references DM (id) on delete restrict on update restrict;
 
-alter table enseignant add constraint FK_Association_7 foreign key (cal_id)
-      references calendrier (id) on delete restrict on update restrict;
+alter table etudiant add constraint FK_Association_23 foreign key (mai_id)
+      references maitre_de_stage (id) on delete restrict on update restrict;
 
-alter table etudiant add constraint FK_Association_9 foreign key (cal_id)
-      references calendrier (id) on delete restrict on update restrict;
+alter table jury add constraint FK_Association_18 foreign key (ens_id)
+      references enseignant (id) on delete restrict on update restrict;
 
-alter table maitre_de_stage add constraint FK_Association_2 foreign key (etu_id)
-      references etudiant (id) on delete restrict on update restrict;
+alter table jury add constraint FK_Association_19 foreign key (mai_id)
+      references maitre_de_stage (id) on delete restrict on update restrict;
 
-alter table president_jury add constraint FK_Association_4 foreign key (sou_id)
+alter table jury add constraint FK_Association_20 foreign key (sou_id)
       references soutenances (id) on delete restrict on update restrict;
 
-alter table president_jury add constraint FK_Association_8 foreign key (cal_id)
-      references calendrier (id) on delete restrict on update restrict;
+alter table jury add constraint FK_Association_21 foreign key (DM_id)
+      references DM (id) on delete restrict on update restrict;
 
-alter table salle add constraint FK_Association_5 foreign key (sou_id)
-      references soutenances (id) on delete restrict on update restrict;
+alter table role add constraint FK_Association_28 foreign key (uti_id)
+      references utilisateur (id) on delete restrict on update restrict;
 
-alter table soutenances add constraint FK_Association_6 foreign key (etu_id)
+alter table soutenances add constraint FK_Association_24 foreign key (pre_id)
+      references president_jury (id) on delete restrict on update restrict;
+
+alter table soutenances add constraint FK_Association_25 foreign key (sal_id)
+      references salle (id) on delete restrict on update restrict;
+
+alter table soutenances add constraint FK_Association_27 foreign key (dem_id)
+      references demande_soutenance (id) on delete restrict on update restrict;
+
+alter table utilisateur add constraint FK_Association_29 foreign key (etu_id)
       references etudiant (id) on delete restrict on update restrict;
 
